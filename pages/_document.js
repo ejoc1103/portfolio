1193b25df4875c287f50f55cd0c15d7c4aaf5614
Ed OConnor <ejoc1103@yahoo.com>
2022-05-25 21:32:47 -0400

    Working on _documnet.js to fix css loading issue

--- pages/_document.js
+++ pages/_document.js
@@ -1,24 +1,29 @@
 // pages/_document.js
 
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
+import { ServerStyleSheet } from 'styled-components';
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     // Run the React rendering logic synchronously
-    ctx.renderPage = () =>
-      originalRenderPage({
-        // Useful for wrapping the whole react tree
-        enhanceApp: App => App,
-        // Useful for wrapping in a per-page basis
-        enhanceComponent: Component => Component,
-      });
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // Useful for wrapping the whole react tree
+          enhanceApp: App => App,
+          // Useful for wrapping in a per-page basis
+          enhanceComponent: Component => Component,
+        });
 
-    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
-    const initialProps = await Document.getInitialProps(ctx);
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+      const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps;
+      return initialProps;
+    } finally {
+      sheet.seal();
+    }
   }
   render() {
     return (
