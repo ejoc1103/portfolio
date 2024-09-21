18f439622c7a0af15ff84648f431f68b0758eb6f
Ed <ejoc1103@gmail.com>
2024-09-20 23:49:48 -0400

    Check

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,6 @@
 
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import '../global.css';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
